4ab56ea87d087c87fe15a506f3f10b4c043c73dd
Mihai Neagoe <mihai@datashaka.com>
2016-06-03 14:16:45 +0100

    Added login, dashboard, createApi routes

--- app.js
+++ app.js
@@ -1,10 +1,48 @@
 var express = require('express');
+var stormpath = require('express-stormpath');
+var config = require('./config/apiKey.json')
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.use(stormpath.init(app, {
+  client: {
+    apiKey: {
+      id: config.apiKeyId,
+      secret: config.apiKeySecret,
+    }
+  },
+  application: {
+    href: 'https://api.stormpath.com/v1/applications/' + config.hrefKey
+  },
+  web: {
+    register: {
+      nextUri: '/dashboard'
+    }
+  }
+}));
+ 
+app.on('stormpath.ready', function () {
+  console.log('Stormpath Ready');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.get('/dashboard', stormpath.loginRequired, function(req, res) {
+  res.send('Welcome back: ' + res.locals.user.email);
 });
+
+app.get('/createKey', stormpath.loginRequired, function (req, res) {
+   account = req.user;
+   
+   account.getApiKeys(function (err, colectionResult) {
+       colectionResult.each(function (apiKey, cb) {
+            apiKey.delete(); 
+            cb();
+       }), function (err) {
+           console.log(err);
+       };
+   });
+   
+   account.createApiKey(function (err, apiKey) {
+        res.send('Your api key has been created </br> ApiId: ' + apiKey.id + '</br> ApiSecret: ' + apiKey.secret );   
+   });
+});
+    
+app.listen(3000);
